7dd64be88ef30b385bb3d838e5ef82099e6ffd99
Jayaharini Srinivasan <jayaharini704@gmail.com>
2025-11-05 14:09:38 +0530

    Day 2 Solution

--- Day2/Solution2.sql
+++ Day2/Solution2.sql
@@ -1,1 +1,15 @@
+--Practise Questions
+
+--Answer 1
+select name from patients where age>0;
+
+--Answer 2
+select staff_name from staff where service="Surgery";
+
+--Answer 3
+select week from services_weekly where patients_request>100;
+
+--Daily Challenge Question
+
+--Answer
 select patient_id,name,age,satisfaction from patients where service="Surgery" and satisfaction<70;
