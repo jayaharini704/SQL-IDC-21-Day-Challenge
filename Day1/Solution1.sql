320a05ae2f59523f09f204afe947bf5159e54f43
Jayaharini Srinivasan <jayaharini704@gmail.com>
2025-11-05 14:12:25 +0530

    Day 1 Solution

--- Day1/Solution1.sql
+++ Day1/Solution1.sql
@@ -1,1 +1,15 @@
+--Practice Questions
+
+--Answer 1
+select * from patients;
+
+--Answer 2
+select patient_id,name,age from patients;
+
+--Answer 3
+select * from services_weekly LIMIT 10;
+
+--Daily Challenge Question
+
+--Answer
 select distinct service from services_weekly;
